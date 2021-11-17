8d8cbfbf266292fd7c8a3fd25b199b11ffabd2c2
MjavadAkbari <86795088+MjavadAkbari@users.noreply.github.com>
2021-11-17 23:06:46 +0330

    adding MainActivity file from amir

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+
+public class MainActivity extends AppCompatActivity {
+  
+  public void onCreate(Bundle saveInstance) {
+    super.onCreate(saveInstance);
+    Toast.make(mainActivity.this,"hello to you ",Toast.LENGTH_LONG).show();
+  
+    
+    
+  }
+  
+  
+  
+}
